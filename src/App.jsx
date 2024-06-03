13c63da485f8918fefd4d467a4a9e93f2c4af5ca
Pavankumarrd10 <doddamanip97@gmail.com>
2024-06-03 21:08:21 +0530

    Updated room.jsx  with a new line

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,8 @@ import EditRoom from "./components/room/EditRoom"
 import ExistingRooms from "./components/room/ExistingRooms"
 import NavBar from "./components/layout/NavBar"
 import Footer from "./components/layout/Footer"
- 
+import RoomListing from "./components/room/RoomListing"
+import Admin from "./components/admin/Admin"
 
 function App() {
   return (
@@ -36,7 +37,8 @@ function App() {
 						<Route path="/edit-room/:roomId" element={<EditRoom />} />
 						<Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/add-room" element={<AddRoom />} />
-
+             <Route path="/browse-all-rooms" element={<RoomListing />} /> 
+             <Route path="/admin" element={<Admin />} />
         {/* Add other routes here */}
       </Routes>
     </Router>
--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useState } from "react"
+import React, { useState } from "react"
 import { NavLink, Link } from "react-router-dom"
- 
+// import Logout from "../auth/Logout"
 
 
 const NavBar = () => {
@@ -39,13 +39,13 @@ const NavBar = () => {
 							</NavLink>
 						</li>
 
-						{isLoggedIn && userRole === "ROLE_ADMIN" && (
+						{/* {isLoggedIn && userRole === "ROLE_ADMIN" && ( */}
 							<li className="nav-item">
 								<NavLink className="nav-link" aria-current="page" to={"/admin"}>
 									Admin
 								</NavLink>
 							</li>
-						)}
+						{/* )} */}
 					</ul>
 
 					<ul className="d-flex navbar-nav">
